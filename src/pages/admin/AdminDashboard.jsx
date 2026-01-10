353bd6bac234171affef3191cc5c06af0bd02edb
Jay Patel <jay190patel@gmail.com>
2026-01-10 23:31:10 +0530

    add alt text to admin dashboard image

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -32,7 +32,7 @@ const AdminDashboard = () => {
                     <div className=" bg-pink-50 py-5 rounded-xl border border-pink-100">
                         {/* image  */}
                         {/* <div className="flex justify-center">
-                            <img src="https://cdn-icons-png.flaticon.com/128/2202/2202112.png" alt="" />
+                            <img src="https://cdn-icons-png.flaticon.com/128/2202/2202112.png" alt="Admin dashboard icon" />
                         </div> */}
                         {/* text  */}
                            <div className="">
