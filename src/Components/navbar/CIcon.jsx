31f5d09b2b6a56773b821da61176214c1d8658a5
Jay Patel <jay190patel@gmail.com>
2026-02-22 22:45:40 +0530

    feat: Add role="status" to cart count badge

--- src/Components/navbar/CIcon.jsx
+++ src/Components/navbar/CIcon.jsx
@@ -15,6 +15,7 @@ const CIcon = ({ totalCartQuantity, ariaLabel = "View cart" }) => {
         <span
           className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center"
           aria-label={`${totalCartQuantity} items in cart`}
+          role="status"
         >
           {totalCartQuantity}
         </span>
