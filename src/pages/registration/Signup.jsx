0396c1dada50536a1035b7b045f929691c6388da
Jay Patel <jay190patel@gmail.com>
2026-03-04 00:51:48 +0530

    feat: Add aria-label to Signup contact number input

--- src/pages/registration/Signup.jsx
+++ src/pages/registration/Signup.jsx
@@ -227,6 +227,7 @@ const Signup = () => {
                             })
                         }}
                         className='bg-pink-50 border border-pink-200 px-2 py-2 w-96 rounded-md outline-none placeholder-pink-200'
+                        aria-label="Contact Number"
                     />
                 </div>
 
