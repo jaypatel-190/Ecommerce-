f2c1fb84db79ddd345d55902d30188c571e1df78
Jay Patel <jay190patel@gmail.com>
2026-03-03 23:30:44 +0530

    feat: Add aria-label to Signup last name input

--- src/pages/registration/Signup.jsx
+++ src/pages/registration/Signup.jsx
@@ -143,6 +143,7 @@ const Signup = () => {
                             })
                         }}
                         className='bg-pink-50 border border-pink-200 px-2 py-2 w-96 rounded-md outline-none placeholder-pink-200'
+                        aria-label="Last Name"
                     />
                 </div>
 
