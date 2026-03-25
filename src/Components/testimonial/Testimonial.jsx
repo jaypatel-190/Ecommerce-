ba87f2a67b10af241fa2dbd47ef2d1a13b56cb50
Jay Patel <jay190patel@gmail.com>
2026-03-25 23:34:54 +0530

    feat: Add role to testimonial images in Testimonial

--- src/Components/testimonial/Testimonial.jsx
+++ src/Components/testimonial/Testimonial.jsx
@@ -116,6 +116,7 @@ const Testimonial = () => {
                     alt={testimonial.altText}
                     className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
                     src={testimonial.image}
+                    role="img"
                   />
                   <p className="leading-relaxed">{testimonial.quote}</p>
                   <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
