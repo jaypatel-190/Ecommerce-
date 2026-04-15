fe3dab5d2e381413315ba3c212018fde3a3380bf
Jay Patel <jay190patel@gmail.com>
2026-04-16 02:00:59 +0530

    feat: Add semantic image attributes to Facebook icon in Footer component

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
         </p>
         <div className="flex justify-center sm:ml-auto mt-4 sm:mt-0">
           <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="mx-2 text-gray-100 cursor-pointer" aria-label="Facebook" tabIndex={0}>
-            <svg fill="currentColor" className="w-5 h-5" viewBox="0 0 24 24">
+            <svg fill="currentColor" className="w-5 h-5" viewBox="0 0 24 24" role="img" aria-hidden="true">
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
             </svg>
           </a>
