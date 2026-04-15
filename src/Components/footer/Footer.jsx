f67555c789184c5a7af517e3709420c4e90a1f51
Jay Patel <jay190patel@gmail.com>
2026-04-16 02:01:36 +0530

    feat: Add semantic image attributes to Instagram icon in Footer component

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -36,6 +36,8 @@ const Footer = () => {
               stroke="currentColor"
               className="w-5 h-5"
               viewBox="0 0 24 24"
+              role="img"
+              aria-hidden="true"
             >
               <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
