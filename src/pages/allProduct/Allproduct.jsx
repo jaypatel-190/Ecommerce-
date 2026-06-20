62689010cb3ce0782ac1aec30eff0a62e296d3c9
Jay Patel <jay190patel@gmail.com>
2026-06-20 23:47:22 +0530

    fix: use unique product id as key in lists instead of array index to prevent render bugs

--- src/pages/allProduct/Allproduct.jsx
+++ src/pages/allProduct/Allproduct.jsx
@@ -60,7 +60,7 @@ const AllProduct = () => {
                 getAllProduct.map((item, index) => {
                   const { id, title, price, productImageUrl } = item;
                 return (
-                  <div key={index} className="p-4 w-full md:w-1/2 lg:w-1/4">
+                  <div key={id} className="p-4 w-full md:w-1/2 lg:w-1/4">
                     <div className="h-full border border-gray-300 rounded-xl overflow-hidden shadow-md cursor-pointer">
                       <img
                         onClick={() => navigate(`/productinfo/${id}`)}
